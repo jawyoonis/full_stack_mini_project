8760e117b81aa29a43c9673a9f534e0cf88ab6cd
vagrant <vagrant@vagrant.vm>
2020-04-26 15:32:38 +0000

    updated the file webserver

--- full_stack_project/webserver.py
+++ full_stack_project/webserver.py
@@ -1,32 +1,63 @@
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import cgi
 
+class WebServerHandler(BaseHTTPRequestHandler):
 
-class webserverHandler(BaseHTTPRequestHandler):
-     def do_Get(self):
+    def do_GET(self):
+        if self.path.endswith("/hello"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = ""
+            message += "<html><body>Hello!</body></html>"
+            self.wfile.write(message)
+            print message
+            return
+	
+	if self.path.endswith("/hola"):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            message = ""
+            message += "<html><body> &#161 Hola ! </body></html>"
+            self.wfile.write(message)
+            print message
+            return
+
+        else:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+    def do_post(self):
 	 try:
-	      if self.path.endswith("/hello"):
-		   self.send_response(200)
-		   self.send_header("Content-type", "text/html")
-		   self.end_headers()
-		   output= ""
-		   output+=("<html><body>Hello!</body></html>")
-		   self.wfile.write(output)
-		   print(output)
-		   return 
-	 except:
-	     self.send_error(404, "File is not found %s" % self.path)
+	   self.send_response(301)
+	   self.send_header('Content-type', 'text/html')
+           self.end_headers()
+           ctype, pdict =cgi.parse_header(
+                  self.headers.getheader('content-type'))
+           if ctype=='multipart/form-data':
+	       fields= cgi.parse_multipart(self.rfile, pdict)
+               messagecontent=fields.get('message')
+           output=""
+           output++"<html><body>"
+           output+= "h<h2> okey, how about this: </h2>"
+           output+="<h2> %s </h1>" % messagecontent[0]
+           output+= '''<form method='POST' enctype='multipart/form-data' action='/hello'> what would you like to say? </h2><input name="message" type="text"> <input type="submit" value="Submit"> </form>'''
+           output+="</body></html>"
+           self.wfile.write(output)
+           print(output)
+
+         except:
+            pass 
+
 
 def main():
     try:
-	port=8080
-	server= HTTPServer(('', port), webserverHandler)
-	print("Web server running on port %s" % port )
-	server.serve_forever()
+        port = 8080
+        server = HTTPServer(('', port), WebServerHandler)
+        print "Web Server running on port %s" % port
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print " ^C entered, stopping web server...."
+        server.socket.close()
 
-    except keyboardInInterrupt:
-	print "^C entered, stopping web server...."
-	server.socket.close()
-
-
-if __name__=='__main__':
-	main()
+if __name__ == '__main__':
+    main()
